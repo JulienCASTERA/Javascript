740aa05cd1c50f6217e11040e4c97c0e16eddf62
JulienCASTERA <julien.castera@ynov.com>
2019-01-28 15:02:23 +0100

    Fail resolved X_X

---
+++ lesson1/02.scripts/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let user;
+let admin;
+
+user = 'Sophie'; // Ici, cette variable est assignée à "Sophie"
+
+admin = user; // On assigne la variable user à admin,
+
+console.log(admin); // Si on demande à la console, elle va afficher Sophie.
+
+const userStatus = 1;
